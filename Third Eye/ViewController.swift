7ea431c9c2b7f02dacf2924054c648c05b0b2956
alessandroparisi <alessandro.parisi@mail.mcgill.ca>
2015-07-18 14:34:19 -0700

    Initial Commit

---
+++ Third Eye/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Third Eye
+//
+//  Created by Alessandro Parisi on 2015-07-18.
+//  Copyright (c) 2015 Trith Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
